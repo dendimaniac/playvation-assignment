2a19281d041fc7cb8a19548b4c0fd8781bbd4acc
Quan Dao <daq30899@gmail.com>
2020-06-30 21:08:57 +0300

    Extract jump functions + caching game control instead of singleton

--- Assets/Flappy Bird Style/Scripts/Bird.cs
+++ Assets/Flappy Bird Style/Scripts/Bird.cs
@@ -2,18 +2,19 @@
 
 public class Bird : MonoBehaviour
 {
-    [SerializeField] private float upForce; //Upward force of the "flap".
+    [SerializeField] private float upForce;
     [SerializeField] private Camera gameCamera;
+    [SerializeField] private GameControl gameControl;
 
-    private bool isDead; //Has the player collided with a wall?
-    private Animator anim; //Reference to the Animator component.
-    private Rigidbody2D rb2d; //Holds a reference to the Rigidbody2D component of the bird.
+    private bool isDead;
+    private Animator anim;
+    private Rigidbody2D rb2d;
     private float topBorder;
 
     private static readonly int Flap = Animator.StringToHash("Flap");
     private static readonly int Die = Animator.StringToHash("Die");
 
-    private void Start()
+    private void Awake()
     {
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
@@ -27,6 +28,11 @@ public class Bird : MonoBehaviour
 
         if (!Input.GetMouseButtonDown(0)) return;
 
+        Jump();
+    }
+
+    private void Jump()
+    {
         anim.SetTrigger(Flap);
         rb2d.velocity = Vector2.zero;
 
@@ -40,6 +46,6 @@ public class Bird : MonoBehaviour
         rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger(Die);
-        GameControl.instance.BirdDied();
+        gameControl.BirdDied();
     }
 }
