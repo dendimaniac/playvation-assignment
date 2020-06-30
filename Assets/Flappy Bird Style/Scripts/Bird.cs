f2ad93c833653cf1e8c0d6512532d9f542a91cab
Quan Dao <daq30899@gmail.com>
2020-06-30 13:40:08 +0300

    fixed bird gets out of top side view

--- Assets/Flappy Bird Style/Scripts/Bird.cs
+++ Assets/Flappy Bird Style/Scripts/Bird.cs
@@ -1,50 +1,59 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Bird : MonoBehaviour 
+public class Bird : MonoBehaviour
 {
-	public float upForce;					//Upward force of the "flap".
-	private bool isDead = false;			//Has the player collided with a wall?
+    [SerializeField] private float upForce; //Upward force of the "flap".
+    [SerializeField] private Camera gameCamera;
 
-	private Animator anim;					//Reference to the Animator component.
-	private Rigidbody2D rb2d;				//Holds a reference to the Rigidbody2D component of the bird.
+    private bool isDead; //Has the player collided with a wall?
+    private Animator anim; //Reference to the Animator component.
+    private Rigidbody2D rb2d; //Holds a reference to the Rigidbody2D component of the bird.
+    private Transform transform;
+    private float topBorder;
 
-	void Start()
-	{
-		//Get reference to the Animator component attached to this GameObject.
-		anim = GetComponent<Animator> ();
-		//Get and store a reference to the Rigidbody2D attached to this GameObject.
-		rb2d = GetComponent<Rigidbody2D>();
-	}
+    private static readonly int Flap = Animator.StringToHash("Flap");
+    private static readonly int Die = Animator.StringToHash("Die");
 
-	void Update()
-	{
-		//Don't allow control if the bird has died.
-		if (isDead == false) 
-		{
-			//Look for input to trigger a "flap".
-			if (Input.GetMouseButtonDown(0)) 
-			{
-				//...tell the animator about it and then...
-				anim.SetTrigger("Flap");
-				//...zero out the birds current y velocity before...
-				rb2d.velocity = Vector2.zero;
-				//	new Vector2(rb2d.velocity.x, 0);
-				//..giving the bird some upward force.
-				rb2d.AddForce(new Vector2(0, upForce));
-			}
-		}
-	}
+    private void Start()
+    {
+        //Get reference to the Animator component attached to this GameObject.
+        anim = GetComponent<Animator>();
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+        topBorder = gameCamera.orthographicSize + gameCamera.transform.position.y -
+                    GetComponent<SpriteRenderer>().bounds.size.y;
+    }
 
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		// Zero out the bird's velocity
-		rb2d.velocity = Vector2.zero;
-		// If the bird collides with something set it to dead...
-		isDead = true;
-		//...tell the Animator about it...
-		anim.SetTrigger ("Die");
-		//...and tell the game control about it.
-		GameControl.instance.BirdDied ();
-	}
+    private void Update()
+    {
+        //Don't allow control if the bird has died.
+        if (isDead) return;
+
+        //Look for input to trigger a "flap".
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        //...tell the animator about it and then...
+        anim.SetTrigger(Flap);
+        //...zero out the birds current y velocity before...
+        rb2d.velocity = Vector2.zero;
+
+        if (transform.position.y >= topBorder) return;
+
+        //	new Vector2(rb2d.velocity.x, 0);
+        //..giving the bird some upward force.
+        rb2d.AddForce(new Vector2(0, upForce));
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        // Zero out the bird's velocity
+        rb2d.velocity = Vector2.zero;
+        // If the bird collides with something set it to dead...
+        isDead = true;
+        //...tell the Animator about it...
+        anim.SetTrigger(Die);
+        //...and tell the game control about it.
+        GameControl.instance.BirdDied();
+    }
 }
