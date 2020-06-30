2e40d12e8618514353937b60ae90348464d8de76
Quan Dao <daq30899@gmail.com>
2020-06-30 13:44:46 +0300

    removed all comments

--- Assets/Flappy Bird Style/Scripts/Bird.cs
+++ Assets/Flappy Bird Style/Scripts/Bird.cs
@@ -16,9 +16,7 @@ public class Bird : MonoBehaviour
 
     private void Start()
     {
-        //Get reference to the Animator component attached to this GameObject.
         anim = GetComponent<Animator>();
-        //Get and store a reference to the Rigidbody2D attached to this GameObject.
         rb2d = GetComponent<Rigidbody2D>();
         transform = GetComponent<Transform>();
         topBorder = gameCamera.orthographicSize + gameCamera.transform.position.y -
@@ -27,33 +25,23 @@ public class Bird : MonoBehaviour
 
     private void Update()
     {
-        //Don't allow control if the bird has died.
         if (isDead) return;
 
-        //Look for input to trigger a "flap".
         if (!Input.GetMouseButtonDown(0)) return;
 
-        //...tell the animator about it and then...
         anim.SetTrigger(Flap);
-        //...zero out the birds current y velocity before...
         rb2d.velocity = Vector2.zero;
 
         if (transform.position.y >= topBorder) return;
 
-        //	new Vector2(rb2d.velocity.x, 0);
-        //..giving the bird some upward force.
         rb2d.AddForce(new Vector2(0, upForce));
     }
 
     private void OnCollisionEnter2D()
     {
-        // Zero out the bird's velocity
         rb2d.velocity = Vector2.zero;
-        // If the bird collides with something set it to dead...
         isDead = true;
-        //...tell the Animator about it...
         anim.SetTrigger(Die);
-        //...and tell the game control about it.
         GameControl.instance.BirdDied();
     }
 }
